91d080aafd9fd60783dc760e9441f9b16de4b984
Greg L <sincore@gmail.com>
2023-05-20 22:31:53 -0400

    wip

--- __init__.py
+++ __init__.py
@@ -1,121 +1,138 @@
-# -*- coding: utf-8 -*-
-
 """Search the Laravel Documentation"""
+from pathlib import Path
 
-from os import path
+from albert import QueryHandler, Item
+
+import os
 import urllib.parse
 import html
 from algoliasearch.search_client import SearchClient
 
-from albert import *
-
-__title__ = "Laravel Docs"
-__prettyname__ = "Laravel Docs"
-__doc__ = "Albert extension for quickly and easily searching the Laravel documentation"
-__version__ = "0.4.1"
-__triggers__ = "ld "
-__authors__ = "Rick West"
-__py_dep__ = ["algoliasearch"]
+md_iid = "0.5"
+md_version = "0.4"
+md_id = __name__
+md_name = "Laravel Docs"
+md_description = "Albert extension for quickly and easily searching the Laravel documentation"
+#md_license = "BSD-2"
+#md_url = "https://url.com/to/upstream/sources/and/maybe/issues"
+#md_maintainers = "@preferrablyYourGithubName"
 
 
 client = SearchClient.create("8BB87I11DE", "8e1d446d61fce359f69cd7c8b86a50de")
 index = client.init_index("docs")
 
+# icon = "{}/icon.png".format(path.dirname(__file__))
+# google_icon = "{}/google.png".format(path.dirname(__file__))
+#
 
-icon = "{}/icon.png".format(path.dirname(__file__))
-google_icon = "{}/google.png".format(path.dirname(__file__))
+GOOGLE_ICON_PATH = str(Path(__file__).parent / 'google.png')
+ICON_PATH = str(Path(__file__).parent / 'icon.png')
 
 docs = "https://laravel.com/docs/"
 
 
-def getSubtitle(hit):
-    if hit["h4"] is not None:
-        return hit["h4"]
 
-    if hit["h3"] is not None:
-        return hit["h3"]
+class Plugin(QueryHandler):
+    def id(self):
+        return md_id
 
-    if hit["h2"] is not None:
-        return hit["h2"]
+    def name(self):
+        return md_name
 
-    return None
+    def description(self):
+        return md_description
 
+    def defaultTrigger(self):
+        return "ld "
 
-def handleQuery(query):
-    items = []
+    def initialize(self):
+        self.icon = [os.path.dirname(__file__) + "/icon.png"]
 
-    if query.isTriggered:
+    def finalize(self):
+        info('finalize')
 
-        if not query.isValid:
-            return
+    def getSubtitle(hit):
+        if hit["h4"] is not None:
+            return hit["h4"]
 
-        if query.string.strip():
-            search = index.search(
-                query.string, {"tagFilters": "master", "hitsPerPage": 5}
-            )
+        if hit["h3"] is not None:
+            return hit["h3"]
 
-            for hit in search["hits"]:
+        if hit["h2"] is not None:
+            return hit["h2"]
 
-                title = hit["h1"]
-                subtitle = getSubtitle(hit)
-                url = "{}{}".format(docs, hit["link"])
+        return None
+    def handleTriggerQuery(self, query):
+        items = []
 
-                text = False
-                try:
-                    text = hit["_highlightResult"]["content"]["value"]
-                except KeyError:
-                    pass
+        if query.isTriggered:
 
-                if text and subtitle:
-                    title = "{} - {}".format(title, subtitle)
-                    subtitle = text
+            if not query.isValid:
+                return
 
-                items.append(
-                    Item(
-                        id=__prettyname__,
-                        icon=icon,
-                        text=html.unescape(title),
-                        subtext=html.unescape(subtitle if subtitle is not None else ""),
-                        actions=[UrlAction("Open in the Laravel Documentation", url)],
+            if query.string.strip():
+                search = index.search(
+                    query.string, {"tagFilters": "master", "hitsPerPage": 5}
+                )
+
+                for hit in search["hits"]:
+
+                    title = hit["h1"]
+                    subtitle = self.getSubtitle(hit)
+                    url = "{}{}".format(docs, hit["link"])
+
+                    text = False
+                    try:
+                        text = hit["_highlightResult"]["content"]["value"]
+                    except KeyError:
+                        pass
+
+                    if text and subtitle:
+                        title = "{} - {}".format(title, subtitle)
+                        subtitle = text
+
+                    items.append(
+                        {
+                            'icon': ICON_PATH,
+                            'text': html.unescape(title),
+                            'subtext': html.unescape(subtitle if subtitle is not None else ""),
+                            'actions': [UrlAction("Open in the Laravel Documentation", url)],
+                        }
                     )
-                )
 
-            if len(items) == 0:
-                term = "laravel {}".format(query.string)
+                if len(items) == 0:
+                    term = "laravel {}".format(query.string)
 
-                google = "https://www.google.com/search?q={}".format(
-                    urllib.parse.quote(term)
-                )
-
-                items.append(
-                    Item(
-                        id=__prettyname__,
-                        icon=google_icon,
-                        text="Search Google",
-                        subtext='No match found. Search Google for: "{}"'.format(term),
-                        actions=[UrlAction("No match found. Search Google", google)],
+                    google = "https://www.google.com/search?q={}".format(
+                        urllib.parse.quote(term)
                     )
-                )
 
-                items.append(
-                    Item(
-                        id=__prettyname__,
-                        icon=icon,
-                        text="Open Docs",
-                        subtext="No match found. Open laravel.com/docs...",
-                        actions=[UrlAction("Open the Laravel Documentation", docs)],
+                    items.append(
+                        {
+                            'icon': GOOGLE_ICON_PATH,
+                            'text': "Search Google",
+                            'subtext': 'No match found. Search Google for: "{}"'.format(term),
+                            'actions': [UrlAction("No match found. Search Google", google)],
+                        }
                     )
+
+                    items.append(
+                        {
+                            'icon': GOOGLE_ICON_PATH,
+                            'text': "Open Docs",
+                            'subtext': "No match found. Open laravel.com/docs...",
+                            'actions': [UrlAction("Open the Laravel Documentation", docs)],
+                        }
+                    )
+
+            else:
+                items.append(
+                    {
+                        'icon': GOOGLE_ICON_PATH,
+                        'text': "Open Docs",
+                        'subtext': "Open laravel.com/docs....",
+                        'actions': [UrlAction("Open the Laravel Documentation", docs)],
+                    }
                 )
 
-        else:
-            items.append(
-                Item(
-                    id=__prettyname__,
-                    icon=icon,
-                    text="Open Docs",
-                    subtext="Open laravel.com/docs...",
-                    actions=[UrlAction("Open the Laravel Documentation", docs)],
-                )
-            )
-
-    return items
+        return items
